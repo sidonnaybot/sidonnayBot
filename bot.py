f82aa997f6782db5dff786705627dbe7af23d02a
sidonnaybot <nanabotnax@gmail.com>
2026-02-17 08:36:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,7 +190,7 @@ def stream_status_loop():
         stream_online = is_stream_online()
 
         if stream_online and not stream_greeted:
-            sock.send(f"PRIVMSG {CHANNEL} :Здравствуйте, Нана🌸\r\n".encode())
+            sock.send(f"PRIVMSG {CHANNEL} :Здравствуйте, Нана 🌸\r\n".encode())
             stream_greeted = True
 
         if not stream_online:
@@ -264,6 +264,6 @@ while True:
     elif msg == "!instagram":
         sock.send(f"PRIVMSG {CHANNEL} :https://instagram.com/sidonnay MorphinTime\r\n".encode())
     elif msg == "!фильм":
-        sock.send(f"PRIVMSG {CHANNEL} :Солдаты неудачи PopCorn\r\n".encode())
+        sock.send(f"PRIVMSG {CHANNEL} :Человек Паук 1 PopCorn\r\n".encode())
     elif msg == "!озвучка":
         sock.send(f"PRIVMSG {CHANNEL} :https://inlnk.ru/DB36GQ\r\n".encode())
